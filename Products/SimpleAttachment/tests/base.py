e9540e481ee4096fa0d7eca8ab243a5332efd4c5
andrewb <andrewb@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-02-03 18:21:12 +0000

    ensure that our zcml configuration and most notably the GenericSetup profile is deferred until the setup of the Plone site testing layer, among other things, this allows us to run the tests suite using roadrunner and saves a lot of setup time
    git-svn-id: https://svn.plone.org/svn/collective/Products.SimpleAttachment/trunk@80276 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/SimpleAttachment/tests/base.py
+++ Products/SimpleAttachment/tests/base.py
@@ -5,14 +5,49 @@
 from Testing import ZopeTestCase
 from Products.PloneTestCase import PloneTestCase
 
-# Make ZopeTestCase aware of the standard products
-ZopeTestCase.installProduct('SimpleAttachment')
+from Products.Five import zcml
+from Products.Five import fiveconfigure
 
-# Set up the Plone site used for the test fixture. The PRODUCTS are the products
-# to install in the Plone site (as opposed to the products defined above, which
-# are all products available to Zope in the test fixture)
-PRODUCTS = ['SimpleAttachment']
-PloneTestCase.setupPloneSite(products=PRODUCTS)
+# Set up the Plone site used for the test fixture.
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_product():
+    """Set up the package and its dependencies.
+
+    The @onsetup decorator causes the execution of this body to be
+    deferred until the setup of the Plone site testing layer. We could
+    have created our own layer, but this is the easiest way for Plone
+    integration tests.
+    """
+
+    # Load the ZCML configuration for the example.tests package.
+    # This can of course use <include /> to include other packages.
+
+    fiveconfigure.debug_mode = True
+    import Products.SimpleAttachment
+    zcml.load_config('configure.zcml', Products.SimpleAttachment)
+    fiveconfigure.debug_mode = False
+
+    # We need to tell the testing framework that these products
+    # should be available. This can't happen until after we have loaded
+    # the ZCML. Thus, we do it here. Note the use of installPackage()
+    # instead of installProduct().
+    # This is *only* necessary for packages outside the Products.*
+    # namespace which are also declared as Zope 2 products, using
+    # <five:registerPackage /> in ZCML.
+
+    # We may also need to load dependencies, e.g.:
+    #   ztc.installPackage('borg.localrole')
+
+    ZopeTestCase.installPackage('Products.SimpleAttachment')
+
+# The order here is important: We first call the (deferred) function
+# which installs the products we need for this product. Then, we let
+# PloneTestCase set up this product on installation.
+
+setup_product()
+PloneTestCase.setupPloneSite(products=['SimpleAttachment'])
 
 class SimpleAttachmentTestCase(PloneTestCase.PloneTestCase):
 
