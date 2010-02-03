33218403294ae0cbd4c348f7903a18c42438e90d
witsch <witsch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-03 21:51:09 +0000

    pyflakes
    git-svn-id: https://svn.plone.org/svn/collective/Products.SimpleAttachment/trunk@110235 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/Products/SimpleAttachment/__init__.py
+++ src/Products/SimpleAttachment/__init__.py
@@ -1,13 +1,17 @@
 from Products.Archetypes.atapi import process_types, listTypes
 from Products.CMFCore import utils, DirectoryView
-from Products.SimpleAttachment.config import *
+from Products.SimpleAttachment.config import PROJECTNAME
+from Products.SimpleAttachment.config import DEFAULT_ADD_CONTENT_PERMISSION
+
 
 DirectoryView.registerDirectory('skins', globals())
+
 
 def initialize(context):
     
     # Import the type, which results in registerType() being called
     from content import FileAttachment, ImageAttachment
+    FileAttachment, ImageAttachment     # make pyflakes happy :)
 
     # initialize the content, including types and add permissions
     content_types, constructors, ftis = process_types(
--- src/Products/SimpleAttachment/widget/__init__.py
+++ src/Products/SimpleAttachment/widget/__init__.py
@@ -1,2 +1,4 @@
 from attachments import AttachmentsManagerWidget
 from images import ImagesManagerWidget
+
+__all__ = (AttachmentsManagerWidget, ImagesManagerWidget)
