82710f31c2c363cc16462c26b755a1daa40f4839
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-08-29 14:03:27 +0000

    Also work around p.a.blob weirdness for file attachments
    git-svn-id: https://svn.plone.org/svn/collective/Products.SimpleAttachment/trunk@243882 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/Products/SimpleAttachment/content/file.py
+++ src/Products/SimpleAttachment/content/file.py
@@ -1,12 +1,14 @@
-from Products.CMFCore.utils import getToolByName
 from logging import getLogger
 
-from zope.interface import implements
-from ZODB.POSException import ConflictError
-
 from AccessControl import ClassSecurityInfo
+from Acquisition import aq_base
 from Products.ATContentTypes.content.file import ATFile
 from Products.Archetypes.public import registerType
+from Products.Archetypes.utils import shasattr
+from Products.CMFCore.utils import getToolByName
+from ZODB.POSException import ConflictError
+from zope.interface import implements
+
 from Products.SimpleAttachment.config import PROJECTNAME
 from Products.SimpleAttachment.interfaces import IFileAttachment
 
@@ -42,5 +44,19 @@ class FileAttachment(ATFile):
         except:
             getLogger(__name__).exception('exception while trying to convert '
                'blob contents to "text/plain" for %r', self)
+
+    def getFilename(self, key=None):
+        """Returns the filename from a field.
+        """
+        if key is None:
+            raw = self.getPrimaryField().getRaw(self)
+            filename = getattr(aq_base(raw), 'filename', None)
+            if filename:
+                return filename
+            return self.getId()
+        else:
+            field = self.getField(key) or getattr(self, key, None)
+            if field and shasattr(field, 'getFilename'):
+                return field.getFilename(self)
 
 registerType(FileAttachment, PROJECTNAME)
