68a0806efe001d2954e7d6a2c9ea72c7ae6ba8c5
petschki <petschki@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-12-12 13:50:35 +0000

    fixed normalizer for file/imagenames with umlauts
    git-svn-id: https://svn.plone.org/svn/collective/Products.SimpleAttachment/trunk@247233 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/Products/SimpleAttachment/filename_normalizer.py
+++ src/Products/SimpleAttachment/filename_normalizer.py
@@ -1,3 +1,4 @@
+from Products.CMFPlone.utils import safe_unicode
 from plone.i18n.normalizer.interfaces import IUserPreferredFileNameNormalizer
 from zope.publisher.browser import BrowserView
 
@@ -5,4 +6,4 @@ from zope.publisher.browser import BrowserView
 class FilenameNormalizer(BrowserView):
 
     def __call__(self, filename):
-        return IUserPreferredFileNameNormalizer(self.request).normalize(filename)
+        return IUserPreferredFileNameNormalizer(self.request).normalize(safe_unicode(filename))
